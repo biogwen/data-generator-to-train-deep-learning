8b07e283ca93004b7b633d8513fae80878343ae6
biogwen <g.mouthuy@gmail.com>
2021-04-28 06:36:20 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# import des bibliothèques
+import pandas as pd
+
+#parcing fichier source
+#2 colonnes, une identification de la personne, l'autre contenant les contenus
+
+
+
+# import des listes de contenu
+
+
+#détermination aléatoire du nombre de contenu par personne
+
+#génération des contenus par personne
+
+#export en excel
+
+
