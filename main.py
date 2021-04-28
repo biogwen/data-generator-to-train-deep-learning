713852c1a477e41eaa747ddffee70ac350303160
quiorte <g.mouthuy@gmail.com>
2021-04-28 17:56:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 import pandas as pd
 from random import randint
 
+
+#création objet client
+class stuff:
+    def __init__(self, type_objet, proprietaire):
+        self.type_objet = type_objet
+        self.proprietaire = proprietaire
+
+
 #parsing fichier source
 #2 colonnes, une identification de la personne, l'autre contenant les contenus
 
@@ -13,6 +21,14 @@ from random import randint
 #détermination aléatoire du nombre de contenu par personne
 
 #génération des contenus par personne
+def add_stuff(self, username, cust_name):
+    num_things = randint(1, 60)
+    i = 0
+
+    for i <= num_things:
+
+
+
 
 #export en excel
 
