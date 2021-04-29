6ff579d684d6b53945cef2c405b94868af6593fd
quiorte <g.mouthuy@gmail.com>
2021-04-29 15:51:08 +0200

    add of prenom.csv

--- main.py
+++ main.py
@@ -3,17 +3,29 @@ import pandas as pd
 from random import randint
 
 
-#création objet client
-stuff = [{"nom":"pierre", "objet": []},]
+#import des prénoms
+listing_prenoms = pd.read_csv('prenom.csv')
+prenom = listing_prenoms.prenom
 
+# création des codes produits à 9 chiffres
 
+produits = []
+for index_produit in range(50):
+    reference_produit = ""
+    for i in range(9):
+        chiffre = randint(0, 9)
+        reference_produit = reference_produit + str(chiffre)
 
-#parsing fichier source
-#2 colonnes, une identification de la personne, l'autre contenant les contenus
+    produits.append(reference_produit)
 
+print (produits)
 
+#création listings
 
-# import des listes de contenu
+stuff = []
+for prenoms in prenom:
+    stuff.append({"nom":prenoms, "objets": []})
+
 
 
 #détermination aléatoire du nombre de contenu par personne
@@ -23,7 +35,7 @@ def add_stuff(self, username, cust_name):
     num_things = randint(1, 60)
     i = 0
 
-    for i <= num_things:
+   # for i <= num_things:
 
 
 #export en excel
